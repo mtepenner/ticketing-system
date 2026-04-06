a1af1dc6e2b57f9a87f52d92a5f30279d4a0d895
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-06 15:32:14 -0700

    Update base.py

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,10 @@
+# Import all the models, so that Base has them before being
+# imported by Alembic's env.py
 
+from app.db.database import Base  # noqa
+from app.models.user import User  # noqa
+from app.models.ticket import Ticket  # noqa
+
+# Now, in your alembic/env.py, you will import Base from THIS file:
+# from app.db.base import Base
+# target_metadata = Base.metadata
