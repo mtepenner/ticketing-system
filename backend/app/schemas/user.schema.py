27a7af33e9084691154e8b4b8d45b3386cb87214
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-06 15:28:06 -0700

    Update user.schema.py

--- backend/app/schemas/user.schema.py
+++ backend/app/schemas/user.schema.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
 
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: UUID
+    is_active: bool
+
+    class Config:
+        from_attributes = True
