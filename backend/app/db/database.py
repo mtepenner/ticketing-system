d3197927c34544872185d661cddc43312b8f148a
Matthew Timothy Erwin Penner <mtepenner@gmail.com>
2026-04-06 15:19:50 -0700

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,1 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# Example: postgresql://user:password@localhost:5432/ticketing_db
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# Dependency to get DB session in our routes
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
